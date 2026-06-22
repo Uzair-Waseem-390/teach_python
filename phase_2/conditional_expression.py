61e90c32e6fb4f55457565492c47a832b2a13ae0
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-06-22 21:22:53 +0500

    phase 2 is done

---
+++ phase_2/conditional_expression.py
@@ -0,0 +1,13 @@
+# here we understand the conditional expression 
+# it is a way to write if-else in a single line
+
+
+# Syntax
+# value_if_true if condition else value_if_false
+
+
+# Example 1: Simple conditional expression
+
+num = int(input("Enter a number: "))
+result = "Positive" if num > 0 else "Not Positive"
+print(result)
