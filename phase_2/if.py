75c157d398f70b2ee7b6664527c4816ec4456fd2
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-06-24 12:32:39 +0500

    editing if.py

--- phase_2/if.py
+++ phase_2/if.py
@@ -2,6 +2,15 @@
 # it is used to make decision 
 # for example if it rains then take umbrella otherwise not
 
+# syntax: 
+#if condition:
+#    statement1
+#    statement2
+#    statement3
+#    ...
+
+# in python code must be indent  by the same number of spaces to show that it is inside the if statement   
+
 age = int(input("Enter your age: "))
 if age >= 18:
     print("You are eligible for voting")
@@ -24,9 +33,9 @@ if age <= 0:
     print("this is not possible")
 
 print("program is still continue till end")
+# exit()
 
-
-
+# Example 2
 number = int(input("Enter a number: "))
 if number > 0:
     print("Positive number")
@@ -38,8 +47,7 @@ if number < 0:
     print("Negative number")
 print("this is after third if")
 
-
-
+# Example 3
 password = str(input("Enter your password: "))
 
 if len(password) >= 8:
