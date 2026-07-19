9c3e0a73d4a029035c019f669b65cc80316456c8
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-07-19 23:10:45 +0500

    adding practice

---
+++ phase_3/practice.py
@@ -0,0 +1,8 @@
+# here are some pracitice questions
+# Q1: separate if statements
+# Q2: if-else
+# Q3: elif chain (temperature classifier)
+# Q4: nested if (age + license check)
+# Q5: match-case (days of week)
+# Q6: conditional/ternary expression
+# Q7: combined challenge mixing nested if + ternary
